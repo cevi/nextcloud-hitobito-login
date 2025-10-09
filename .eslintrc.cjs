9f7f80ab20fa105204233274c614b9f08de0f6c6
Cyrill <cyrill.steinmetz@gmail.com>
2025-10-09 23:29:52 +0200

    chore: upgrade to Vue 3

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,7 +1,5 @@
 module.exports = {
-	extends: [
-		'@nextcloud',
-	],
+	extends: ['@nextcloud/eslint-config/vue3', 'prettier'],
 	rules: {
 		'jsdoc/require-jsdoc': 'off',
 		'vue/first-attribute-linebreak': 'off',
--- stylelint.config.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-	rules: {},
-	extends: 'stylelint-config-recommended-vue'
-}
